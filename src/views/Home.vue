78d1681d98b35e644943cd503bcec165a87bf1f0
Pattarawan26034543 <Pattarawan26034543@gmail.com>
2020-08-06 16:17:34 +0700

    #5 ทดสอบVuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
      <v-btn color="primary">Home</v-btn>
-     <v-btn color="error">Home</v-btn>
+     <v-btn color="error">Error</v-btn>
      <v-btn icon color="pink">
               <v-icon>mdi-heart</v-icon>
             </v-btn>
